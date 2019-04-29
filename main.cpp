d77caf8d87b4e3ade51a71e4e08f8fe9bf91cf90
LPJworkroom <32101892+LPJworkroom@users.noreply.github.com>
2019-04-29 10:04:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,36 +12,23 @@
 #include<string>
 #include<queue>
 #include<vector>
-#include<thread>
-#include<algorithm>
+//#include<thread>
 #include<sstream>	//alternate of itoa&&atoi
 using namespace std;
 #define SOCK int
-const int useport=80,bufsize=10000;
+const int useport=80,bufsize=1000;
+const char myserverip[]="62.234.151.216";
 const string pagebreak="\n------------------------------------------------\n";
-string mobiles[7]={"android","webos","iphone","ipod","blackberry","iemobile","opera mini"};
-struct waitnode{
-	sockaddr addr;
-	SOCK connfd;
-};
-struct timeval timeout={0,1000*100};
 
-queue<SOCK> sockq;	//store recved request socket
-
-
-bool ismobile(string& todo);
-char mytolower(char a);
-void take_request();
-void take_file(SOCK connfd,string& strrecv);
+void doweb(SOCK connfd);
 void server_static(SOCK connfd,ifstream& fin,string& filepath);
 string itoa(int i);
 void send_error(SOCK connfd,string& answer);
 void get_filetype(string& filepath,string& filetype);
 void read_request(string& todo,string& answer,string& filepath);
 void server_initial(int&);
-int recv_write(SOCK connfd,string& dest);
+void recv_write(SOCK connfd,string& dest);
 void clienterror(string& dest,string code,string shortmsg,string longmsg);
-void set_recv_tout(SOCK connfd);
 
 int main()
 {
@@ -50,52 +37,31 @@ int main()
 	sockaddr clientaddr;
 	unsigned clientaddrlen=sizeof(clientaddr);
 	printf("listen..\n");
-	
-	thread doweb(take_request);
-	doweb.detach();
-	
 	int connfd;
 	while ((connfd=accept(s_listen,(sockaddr*)&clientaddr,&clientaddrlen))>0)
 	{
-		sockq.push(connfd);
+		printf("new acc..\n");
+		doweb(connfd);
+		printf("wait for next client...\n");
 	}
 	printf("warning:listen terminated\n");
 return 0;
 }
 
 /*in another thread*/
-void take_request()
+void doweb(SOCK connfd)
 {
-	while (1)
-	{
-		if (sockq.empty()){
-			usleep(1000*100);
-			continue;
-		}
-		int connfd=sockq.front();sockq.pop();
-		string strrecv;
-		recv_write(connfd,strrecv);
-		cout<<"string recv:"<<endl<<strrecv<<pagebreak;	
-		take_file(connfd,strrecv);
-		close(connfd);
-	}
-}
-
-void take_file(SOCK connfd,string& strrecv)
-{
+	string strrecv,answer;
+	/*read request*/
+	recv_write(connfd,strrecv);
+	cout<<"string recv:"<<endl<<strrecv<<pagebreak;
 	/*parse string recved and check file*/
-	string filepath,answer;
+	string filepath;
 	read_request(strrecv,answer,filepath);
 	cout<<"request filepath:"<<endl<<filepath<<pagebreak;
-	ifstream fin;
-	fin.open(filepath); 
+	ifstream fin(filepath);
 	/*file cannot be fetch*/
-	if (answer.empty()&&!fin.is_open()){
-		sleep(1);
-		fin.open(filepath);
-		if (!fin.is_open())
-			clienterror(answer,"404","Oops！","TinyServer正忙！\n请刷新本页面，或检查URL是否正确。\n(x_x)\n");
-	}
+	if (answer.empty()&&!fin.is_open())	clienterror(answer,"404","Not found","Tiny couldn't find the file");
 	/*success*/
 	if (fin.is_open()&&!filepath.empty()){
 		cout<<"prepare static content.."<<pagebreak;
@@ -105,7 +71,13 @@ void take_file(SOCK connfd,string& strrecv)
 		cout<<"wrong request,reply about error"<<pagebreak;
 		send(connfd,answer.c_str(),answer.size(),0);
 	}
-	if (fin.is_open())	fin.close(); 
+	
+	//close(connfd);
+	/*shutdown() is needed:close() only close socket
+	 *in this thread,socket main thread is not closed,
+	 *while shutdown close socket in all thread.
+	 */
+	shutdown(connfd,SHUT_RDWR);	//shut both write and read
 }
 
 void clienterror(string& dest,string code,string shortmsg,string longmsg)
@@ -130,17 +102,12 @@ void server_static(SOCK connfd,ifstream& fin,string& filepath)
 	
 	output+="HTTP/1.0 200 OK\r\n";
 	output+="Server:Tiny web server\r\n";
-	/*short connect or keep-alive?*/
 	output+="Connection:close\r\n";
 	output+="Content-length:"+itoa(text.size())+"\r\n";
 	output+="Content-type:"+filetype+"\r\n\r\n";
 	output+=text;
 	
-	if (send(connfd,output.c_str(),output.size(),0)<0){
-		cout<<"send static content failed."<<endl;
-		perror("send");
-		cout<<pagebreak;
-	}
+	send(connfd,output.c_str(),output.size(),0);
 }
 
 string itoa(int i)
@@ -166,90 +133,37 @@ void get_filetype(string& filepath,string& filetype)
 
 void read_request(string& todo,string& answer,string& filepath)
 {
-	if (todo.empty())	return;
 	int lbound=todo.find("GET");
 	if (lbound==string::npos){
 		clienterror(answer,"501","Not implemented","Tiny doesn't implement this method");
 		return;
 	}
+	//lbound+=strlen("GET");
 	lbound=todo.find('/',lbound);
 	int rbound=todo.find(' ',lbound);
+	filepath='.';
 	filepath+=todo.substr(lbound,rbound-lbound);
-	/*default: index
-	 *special check for mobile agent
-	 */ 
+	/*default: index*/
 	if (filepath.back()=='/')	filepath+="index.html";
-	if (ismobile(todo))	filepath="./mobile"+filepath;
-	else				filepath="."+filepath;
 	return;
 }
 
-bool ismobile(string& todo)
-{	
-	string tmp=todo;
-	int l=tmp.find("User-Agent:"),r=tmp.find("\r\n",l);
-	tmp=tmp.substr(l,r-l);
-	transform(tmp.begin(),tmp.end(),tmp.begin(),mytolower);
-	cout<<"transformed string:"<<tmp<<pagebreak;
-	for (int i=0;i<7;i++)
-	{
-		if (tmp.find(mobiles[i])!=string::npos){
-		//	cout<<"clinet is using "<<mobiles[i]<<pagebreak;
-			return true;
-		}
-	}
-	return false;
-}
-
-char mytolower(char a)
-{
-	if (a>='A'&&a<='Z')	return (a+('a'-'A'));
-	return a;
-}
-
-void set_recv_tout(SOCK connfd)
-{
-	int ret=setsockopt(connfd,SOL_SOCKET,SO_RCVTIMEO,(const char*)&timeout,sizeof(timeout));
-	if (ret<0){
-		cout<<"wrong setsockopt recv";
-		perror("setsockopt");
-		cout<<pagebreak;
-	}
-}
-
-int recv_write(SOCK connfd,string& dest)
+void recv_write(SOCK connfd,string& dest)
 {
 	int len,recv_size=0;
 	char tbuf[bufsize];
 	memset(tbuf,0,sizeof(tbuf));
-	dest.clear();
-	
-	set_recv_tout(connfd);
-	
-	while ((len=recv(connfd,tbuf,bufsize-1,0))>=0)
+	while ((len=recv(connfd,tbuf,bufsize-1,0))>0)
 	{
 		printf("%d\n",len);
-		if (len==0){
-			cout<<"I think client closed connection."<<pagebreak;
-	//		close(connfd);
-			break;
-		}
 		string tmp(tbuf,len);
 		dest+=tmp;
 		recv_size+=len;
-		if (dest.find("\r\n\r\n",max(recv_size-2*bufsize,0))!=string::npos){
+		if (dest.find("\r\n\r\n",max(len-2*bufsize,0))!=string::npos){
 			break;
 		}
-		
 	}
-	if (len<0){
-		cout<<"wrong recv.";
-		perror("recv");
-		cout<<pagebreak;
-		return len;
-	}
-	printf("over.recv size:%d\n",recv_size);
-	return recv_size;
+	printf("over.recv size:%d\n",len);
 }
 
 void server_initial(int& s_listen)
